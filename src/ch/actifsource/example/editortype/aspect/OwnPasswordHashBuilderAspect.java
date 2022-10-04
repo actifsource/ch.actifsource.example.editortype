62c3738cc675184e5201013bb1e63a7c64d8161c
ruti <grischa.rutishauser@actifsource.com>
2022-10-04 15:55:21 +0200

    Support password field

--- src/ch/actifsource/example/editortype/aspect/OwnPasswordHashBuilderAspect.java
+++ src/ch/actifsource/example/editortype/aspect/OwnPasswordHashBuilderAspect.java
@@ -13,8 +13,11 @@ import ch.actifsource.ui.component.child.IChild;
 import ch.actifsource.ui.component.impl.CustomText;
 import ch.actifsource.ui.component.impl.IDynamicStatementElementTextContainer;
 import ch.actifsource.ui.component.impl.IStatementElementTextContainer;
+import ch.actifsource.ui.widget.IGuiText;
 import ch.actifsource.ui.widget.text.contentassist.IContentAssistFactory;
 import ch.actifsource.util.character.HashCodeUtil;
+import org.eclipse.draw2d.*;
+import ch.actifsource.core.Literal;
 
 /**
  * Define the border builder aspect.
@@ -32,6 +35,30 @@ public class OwnPasswordHashBuilderAspect extends AbstractDynamicControlBuilderA
       @Override
       protected String converToModel(String str) {
         return HashCodeUtil.hashCode_SHA1(str, null);
+      }
+      
+      
+      @Override
+      public IGuiText<IChild> getGuiElement() {
+        boolean isGuiElementInitialized = hasGuiElement();
+        IGuiText<IChild> text = super.getGuiElement();
+        
+        if (!isGuiElementInitialized) {
+          text.addFocusListener(new FocusListener() {
+            
+            @Override
+            public void focusGained(FocusEvent e) {
+              getEditorModel().save(new Literal(""), true);
+              updateRepresentation();
+            }
+            
+            @Override
+            public void focusLost(FocusEvent e) {
+              
+            }
+          });
+        }
+        return text;
       }
       
     };
